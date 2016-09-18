9d698ba7b32511637ef9db29826f961452c575da
Schyler Hosler <schylerhosler@gmail.com>
2016-09-18 14:42:33 -0400

    html form will now add new submissions of cat info

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
     })
   })
 });
+
+$('form.cat-form').submit(function(event){
+  event.preventDefault()
+  // could use $('#cat-breed').val() to get value
+  // var valuesArr = $(this).serializeArray();
+  var catName = $('#cat-name').val()
+  var catTemperament = $('#cat-temperament').val()
+  var catBreed = $('#cat-breed').val()
+  var catWeight = $('#cat-weight').val()
+  var values = {cat: {name: catName, breed: catBreed, temperament: catTemperament, weight: catWeight}}
+  var posting = $.post('http://localhost:3000/api/v1/cats', values)
+  posting.done(function(data){
+    // debugger;
+    $(".cats-index").append(`<li>${data.name} is ${data.temperament}</li>`)
+  })
+})
